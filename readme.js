adcdaa1ac25521672ca5926b31d29d17a76ee2ba
Your Name <realfasih@gmail.com>
2022-01-13 22:31:22 +0500

    changes

---
+++ readme.js
@@ -0,0 +1,50 @@
+var a=3,b=4;
+console.log(a+b);
+// # git init---->(for making local folde into repositry)
+
+// # git status --->(for checking status)
+
+// # Untracked files ---> git abhi infiles ko watch nhi kar rhaa
+
+// #  git add .(for making files trackable) Or
+// # ---> se git infiles ko watch karna start kardee gaa
+
+
+// #  first time for account required
+// # $git config --global user.name " apna user name "
+
+// # $git config --global user.email " apni email "
+
+// # git commit -m "Jo kaam apnay kya hay"or specifyy the messege
+
+// # $ git remote add origin "link" --->
+// #  used for local repostry and cloud repositry ko merge karne  ke liyee used karte ha
+//remote Repository  is like a github for srorage
+// # $ git push -u origin master
+// # 1. push-->local repo se cloud repo me code bejh rha ha
+// # 2. -u -->local cloud repositry ko track kar rha hota ha
+// # 3. origin --> origin hamara cloud ha or name of branch or name of remote
+// # 4. master -->master wo branch ha jis ke upper hamra code deploy hoga or name of branch
+
+// git log --> hmme batata ha ke kab hmane  last commit kea ha date or time kke saat
+
+//  git diff  file ka name  ---> hmm check karsakte ha ke kia difference / changes ha last or current work me
+
+//  git checkout file name --> yeh hamme last work par leejata ha :like  same as(CTRL +Z command)
+///                               matlab jo hmane changes kii hogii usko nhi karee ga last changes ko restore kar de gaa
+
+
+// $git clone  file name ---> github repositry desktop par lane ke liyee
+
+//----------------------------------------
+
+// git branch file name ==> for creating new branch
+
+//git branch => for check how many branches do you have & in which branch you use/login now
+   * -> indicates the current branch you use
+
+// git checkout branch name => then you switch to (branch name as you written) branch=> matlab jo apne branch bani ha ap us me aa jao gee
+
+//for merging (files or anything)use-> 
+// $ git merge branch name
+
